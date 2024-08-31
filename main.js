318f43339d7a4fbd49ce2023f299ab440261474d
rafatrr <105844075+rafatrr@users.noreply.github.com>
2024-08-31 12:57:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".contaier");
+function phons(x){
+    img.src = x;
+}
+function colors(color){
+    container.style.background = color;
+}
